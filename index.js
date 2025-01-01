2ba1d8e559bc7236bacbe4974892d4a10af9f985
Dorcus <dorcus@gmail.com>
2025-01-01 22:11:51 +0200

    Added the scissors section to the HTML file.

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ function playGame(playerMove) {
   } else if (computerMove === 'scissors') 
     result = 'You win.'; {
   }
-  }
-
-  else if (playerMove === 'paper') {
+  
+  } else if (playerMove === 'paper') {
     if (computerMove === 'rock') {
     result = 'You win.';
   } else if (computerMove === 'paper') {
@@ -21,5 +20,39 @@ function playGame(playerMove) {
   } else if (computerMove === 'scissors') 
     result = 'You lose.'; {
   }
+
+
+  } else if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You win.'
+    } else if (computerMove === 'scissors') {
+      result = 'Tie';
+    }
+  }
+
+  if (result === 'You win') {
+    score.wins += 1;
+  } else if (result === 'You lose') {
+    score.l0sses += 1;
+  } else if (result === 'Tie') {
+    score.ties += 1;
+  }
+
+  alert(`You picked ${playerMove}. 
+    Computer picked ${computerMove}. ${result}
+    wins: ${score.wins}, 
+    Losses: ${score.losses},
+    Ties: ${score.ties}`);
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock'
   }
 }
